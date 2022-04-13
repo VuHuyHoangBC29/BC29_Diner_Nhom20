eba166d561d8e4f10e69d1d9e594afadd56e7893
VuHuyHoangBC29 <demigodkiller912@gmail.com>
2022-04-13 10:30:04 +0700

    edit responsive for categories

--- js/main.js
+++ js/main.js
@@ -1,12 +1,11 @@
-let isDark = false
-
 document.getElementById("mode__switch").onclick = function () {
-    if(isDark){
-        document.getElementById("home").className = "dark";
-    }else{
-        document.getElementById("home").className = "light";
-        
-    }
-      
-
+    // document.getElementById("search__bar").style.backgroundColor = "black";
+    document.getElementById("search__bar").classList.add("search__bar__dark");
+    document.getElementById("mode__switch").className = "far fa-sun";
 }
+
+// document.getElementById("mode__switch").onclick = function () {
+//     document.getElementById("search__bar").classList.add("search__bar__light");
+//     document.getElementById("search__bar").classList.remove("search__bar__dark")
+//     document.getElementById("mode__switch").className = "far fa-sun";
+// }
