4d59ee08327a07ae9d2b0a96c4ef032f93bae999
VuHuyHoangBC29 <demigodkiller912@gmail.com>
2022-05-02 16:55:40 +0700

    update js

--- js/main.js
+++ js/main.js
@@ -813,4 +813,6 @@ document.getElementById("navbar__icon").onclick = function () {
     document.getElementById('categories').classList.toggle('dark');
     document.getElementById('menu').classList.toggle('dark');
     document.getElementById('jumping').classList.toggle('dark');
+    document.getElementById('video').classList.toggle('dark');
+    document.getElementById('blog').classList.toggle('dark');
 }
