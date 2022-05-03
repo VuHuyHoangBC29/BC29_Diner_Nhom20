c9d126228f89d826d5e08bcd1fe961d8f6e9caa8
VuHuyHoangBC29 <demigodkiller912@gmail.com>
2022-05-03 11:58:22 +0700

    update js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 document.getElementById("navbar__icon").onclick = function () {
+    if (document.getElementById("mode__switch").classList.contains("fa-moon")) {
+        document.getElementById("mode__switch").classList.remove("fa-moon");
+        document.getElementById("mode__switch").classList.add("fa-sun");
+    }
+    else {
+        document.getElementById("mode__switch").classList.add("fa-moon");
+        document.getElementById("mode__switch").classList.remove("fa-sun");
+    }
+    
     document.getElementById('search__bar').classList.toggle('dark');
     document.getElementById('about').classList.toggle('dark');
     document.getElementById('categories').classList.toggle('dark');
